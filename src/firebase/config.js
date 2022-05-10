8d2f3f3e7d72d81d642b1ea64172a54762891715
joseMeneghetti <jrmp.dev@gmail.com>
2022-05-10 17:12:53 -0300

    fix env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,25 +7,18 @@ import { initializeApp, getApps } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.APP_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   databaseURL: process.env.DATABASE_URL,
-//   projectId:process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE,
-//   messagingSenderId: process.env.MESSAGING,
-//   appId: process.env.APP_ID,
-//   measurementId: process.env.MEASUREMENT_iD,
-// };
+
+console.log('process.env.FIREBASE_APPKEY', process.env.NEXT_PUBLIC_FIREBASE_APPKEY)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA3F-TBTTB1nx8umqsJfbIeAP0JJNXOWyc",
-  authDomain: "offroad-market.firebaseapp.com",
-  databaseURL: "https://offroad-market-default-rtdb.firebaseio.com",
-  projectId: "offroad-market",
-  storageBucket: "offroad-market.appspot.com",
-  messagingSenderId: "331767784872",
-  appId: "1:331767784872:web:93c7904685ff650be7699d",
-  measurementId: "G-0B7V7M8NKB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APPKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_URL,
+  projectId:process.env.NEXT_PUBLIC_FIREBASE_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
