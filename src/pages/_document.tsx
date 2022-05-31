e05507989f96a3596ea70783829335e677fc7062
joseMeneghetti <jrmp.dev@gmail.com>
2022-05-30 23:22:30 -0300

    meta + profile init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,13 +45,78 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+          <meta name="language" content="pt-BR" />
+          <title>Roia Shop - Motos e Equipamentos de Trilha</title>
+          <meta
+            name="description"
+            content="Venda de motos de trilha, equipamentos de trilha usados, novos e semi novos."
+          />
+          <meta name="robots" content="none" />
+          <meta name="author" content="JRMPDEV" />
+          <meta
+            name="keywords"
+            content="moto de trilha, trilha, equipamentos de trilha, moto, equipamento, usados, trila, novos"
+          />
+
+          <meta property="og:type" content="page" />
+          <meta property="og:url" content="https://offroad-next.vercel.app/" />
+          <meta
+            property="og:title"
+            content="Roia Shop - Motos e Equipamentos de Trilha"
+          />
+          <meta
+            property="og:image"
+            content="https://firebasestorage.googleapis.com/v0/b/offroad-market.appspot.com/o/ADMIN%2Fimage_processing20200704-17829-1plnmw8.png?alt=media"
+          />
+          <meta
+            property="og:description"
+            content="Venda de motos de trilha, equipamentos de trilha usados, novos e semi novos."
+          />
+
+          <meta property="article:author" content="jrmp.dev" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@" />
+          <meta
+            name="twitter:title"
+            content="Roia Shop - Motos e Equipamentos de Trilha"
+          />
+          <meta name="twitter:creator" content="@" />
+          <meta
+            name="twitter:description"
+            content="Venda de motos de trilha, equipamentos de trilha usados, novos e semi novos."
+          />
 
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
             rel="stylesheet"
           />
 
-          <link rel="icon" href="https://rocketseat.com.br/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favico/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favico/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favico/favicon-16x16.png"
+          />
+          <link rel="manifest" href="favico/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="favico/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import { Phone } from 'phosphor-react'
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr'
 import ForceLoading from '../components/Authentication/ForceLoading'
-import Menu from '../components/Menu/Menu'
+import ProfileInput from '../components/Profile/ProfileInput'
 import useAuth from '../data/hook/useAuth'
+import { ProfileForm } from '../styles/components/Profile/ProfileInput'
 import { LoginContainer } from '../styles/pages/Login'
 
 const Profile: React.FC = ({}) => {
-  const { logout } = useAuth()
+  const { logout, user } = useAuth()
+
+  const fetcher = url => fetch(url).then(r => r.json())
+
+  const { data, error } = useSWR(`/api/user/find-all/${user?.email}`, fetcher)
+
+  const [profile, setProfile] = useState({
+    id: '',
+    email: '',
+    name: ' ',
+    phone: '',
+    cep: '',
+    city: '',
+    state: ''
+  })
+
+  useEffect(() => {
+    if (profile?.cep?.length === 8) {
+      fetch(`https://viacep.com.br/ws/${profile?.cep}/json/`)
+        .then(async response => {
+          const json = await response.json()
+          setProfile({
+            ...profile,
+            city: json.localidade,
+            state: json.uf
+          })
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }, [profile?.cep])
+
+  useEffect(() => {
+    setProfile(data)
+  }, [data])
 
   return (
     <ForceLoading>
@@ -17,6 +55,56 @@ const Profile: React.FC = ({}) => {
           />
         </div>
         <div className="form">
+          <ProfileForm>
+            <ProfileInput
+              /* showError={showError} */
+              type="text"
+              label="email"
+              name="email"
+              value={profile}
+              changeValue={setProfile}
+              disabled
+            />
+            <ProfileInput
+              type="text"
+              label="name"
+              name="name"
+              value={profile}
+              changeValue={setProfile}
+            />
+            <div className="row">
+              <ProfileInput
+                type="text"
+                label="phone"
+                name="phone"
+                value={profile}
+                changeValue={setProfile}
+              />
+              <ProfileInput
+                type="text"
+                label="cep"
+                name="cep"
+                value={profile}
+                changeValue={setProfile}
+              />
+            </div>
+            <div className="row">
+              <ProfileInput
+                type="text"
+                label="city"
+                name="city"
+                value={profile}
+                changeValue={setProfile}
+              />
+              <ProfileInput
+                type="text"
+                label="state"
+                name="state"
+                value={profile}
+                changeValue={setProfile}
+              />
+            </div>
+          </ProfileForm>
           <button onClick={logout}> SAIR </button>
         </div>
       </LoginContainer>
